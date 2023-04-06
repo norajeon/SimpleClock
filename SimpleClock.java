438e34b57b65799c1d90d477e85c18c5ccc398cc
nora <sxj042@gmail.com>
2023-04-06 16:09:35 -0400

    help

--- SimpleClock.java
+++ SimpleClock.java
@@ -7,7 +7,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import static javax.swing.UIManager.getBorder;
 
@@ -18,6 +21,9 @@ public class SimpleClock extends JFrame {
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
+
+        JButton militaryTime;
+        JButton estTime;
     
         JLabel timeLabel;
         JLabel dayLabel;
@@ -26,19 +32,19 @@ public class SimpleClock extends JFrame {
         String day;
         String date;
 
+        boolean isMilitary = true;
+        boolean isEST = true;
+
         SimpleClock() {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 200);
+            this.setSize(400, 300);
             this.setResizable(false);
-            this.setBounds(300,300,600,300);
+//            this.setBounds(300,300,600,300);
             this.setVisible(true);
 
 
-            JButton time = new JButton("12/24 Hour");
-            time.setBounds(50,100,100,30);
-            this.add(time);
 
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
@@ -54,31 +60,75 @@ public class SimpleClock extends JFrame {
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
-    
+
+
+
+            this.militaryTime = new JButton("12/24 Hour");
+//            militaryTime.setBounds(50,100,100,30);
+            militaryTime.addActionListener(this::militaryStandard);
+
+            this.estTime = new JButton("Local/GMT");
+            estTime.addActionListener(this::estTime);
+
+//            ZonedDateTime zdt = ZonedDateTime.ofInstant(ZoneId.systemDefault());
+
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+
             this.setVisible(true);
+            this.add(militaryTime);
+            this.add(estTime);
     
             setTimer();
 
 
             //button
+//            time.addActionListener(new java.awt.event.ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+////                    jButton24H(evt);
+//                    time.setVisible(true);
+////                    contentPane.add(time,BorderLayout.CENTER);
+//
+//                }
+//            });
 
 
-            time.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    timeFormat = new SimpleDateFormat("HH:mm:ss a");
-                    time.setVisible(true);
-//                    contentPane.add(time,BorderLayout.CENTER);
-
-                }
-            });
 
 
         }
+
+    private void estTime(ActionEvent actionEvent) {
+            if (isEST) {
+                timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
+//                timeFormat = TimeZone.getTimeZone("GMT");
+//                gmt.setTimeZone(TimeZone.getTimeZone("GMT+6:00"));
+                isEST = false;
+            } else {
+                timeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                isEST = true;
+            }
+    }
+
+    private void militaryStandard(ActionEvent actionEvent) {
+            if (isMilitary) {
+            timeFormat = new SimpleDateFormat("HH:mm:ss a");
+            isMilitary = false;
+            } else {
+                timeFormat = new SimpleDateFormat("hh:mm:ss a");
+                isMilitary = true;
+            }
+
+
+    }
+
+//    public void jButton24H(java.awt.event.ActionEvent evt) {
+//            timeFormat = new SimpleDateFormat("HH:mm:ss a");
+//        }
+//
+
 
         public void setTimer() {
             while (true) {
